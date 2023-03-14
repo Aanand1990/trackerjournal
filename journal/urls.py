49f0c0adfff6fe9018ac5a278e460cce8476aa21
Aanand Manohar <me@aanandmanohar.com>
2023-03-14 19:59:36 +0530

    added templates

--- journal/urls.py
+++ journal/urls.py
@@ -13,12 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls.static import static
-from django.conf import settings
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('journal.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('/', views.home, name = 'home')
+]
--- trackerblog/settings.py
+++ trackerblog/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'trackerblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['trackerblog/templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
