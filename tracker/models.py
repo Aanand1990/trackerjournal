ea1612d58b1b740f960621f33b1f8a69c1a169fb
Aanand Manohar <me@aanandmanohar.com>
2023-03-14 19:20:03 +0530

    Added date component to tracker app

--- tracker/models.py
+++ tracker/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class TrackerApp(models.Model):
+    date = models.DateTimeField(auto_now_add=True, blank=True)
     weight = models.FloatField()
     neck = models.FloatField()
     arms = models.FloatField()
