745cf241c70ab97460820f7f08741893a5706afa
Aanand Manohar <me@aanandmanohar.com>
2023-03-15 22:05:35 +0530

    added title to model show

--- journal/models.py
+++ journal/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+
 class JournalBlog(models.Model):
     date = models.DateTimeField(auto_now_add=True, blank=True)
+    title = models.CharField(max_length=200)
     body = models.TextField()
 
+    def __str__(self):
+        return self.title
