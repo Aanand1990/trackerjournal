9b3f90c23f200810e1015731bb4572c36d799fb5
Aanand Manohar <me@aanandmanohar.com>
2023-03-15 21:50:30 +0530

    Added CSRF trusted origins

--- trackerblog/settings.py
+++ trackerblog/settings.py
@@ -29,7 +29,10 @@ ALLOWED_HOSTS = ['d32f7036-b567-478e-8a29-2bae25d88012.id.repl.co',
                  'journal.aanand.blog',
                  'trackerjournal-1.pradnyanandjadh.repl.co',]
 
-
+CSRF_TRUSTED_ORIGINS=['https://d32f7036-b567-478e-8a29-2bae25d88012.id.repl.co',
+                        'https://journal.aanand.blog',
+                        'trackerjournal-1.pradnyanandjadh.repl.co',
+                      ]
 # Application definition
 
 INSTALLED_APPS = [
