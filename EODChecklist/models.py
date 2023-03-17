45d85013ef34de8f09f4ed7a08af3c406596622e
Aanand Manohar <me@aanandmanohar.com>
2023-03-17 07:45:49 +0530

    Added title to EOD Checklist

--- EODChecklist/models.py
+++ EODChecklist/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class EODChecklist(models.Model):
+    title = models.CharField(max_length=200)
     date = models.DateTimeField(auto_now_add=True, blank=True)
     gym_workout = models.BooleanField(default=False)
     reading_50_pages = models.BooleanField(default=False)
